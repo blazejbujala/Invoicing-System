2e9a33ad83e1ae9893c3b35a995fe9f93ee1f11d
Blazej.Bujala <b.bujala@gmail.com>
2022-04-27 12:11:06 +0200

    Modified Angular

--- angular-frontend/Invoicing-System-web/src/app/components/company-list/company-list.component.ts
+++ angular-frontend/Invoicing-System-web/src/app/components/company-list/company-list.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {CompanyDto} from "../../model/company.dto";
 import {CompanyService} from "../../services/company.service";
+import {faTrash} from '@fortawesome/free-solid-svg-icons';
+import {ToastrService} from "ngx-toastr";
 import {Router} from "@angular/router";
 
 @Component({
@@ -11,8 +13,9 @@ import {Router} from "@angular/router";
 export class CompanyListComponent implements OnInit {
 
   companies: Array<CompanyDto> = []
+  faTrash = faTrash;
 
-  constructor(private companyService: CompanyService, private router: Router) {
+  constructor(private companyService: CompanyService, private router: Router, private toastService: ToastrService,) {
   }
 
   ngOnInit(): void {
@@ -32,4 +35,14 @@ export class CompanyListComponent implements OnInit {
   navigateToPreview(id: string){
   this.router.navigate(['companies',id])
   }
+
+   delete(id: string) {
+      this.companyService.delete(id).subscribe(data => {
+          this.toastService.success("Company deleted"),
+            this.router.navigate(['companies'])
+        },
+        error => {
+          this.toastService.error("Something went wrong")
+        })
+    }
 }
